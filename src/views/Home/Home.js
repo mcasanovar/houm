0f1d7838a366f1c32e0f6497ac4b8dbc6103bf94
mcasanovar <mariocasanovaramirez@gmail.com>
2022-05-07 16:09:19 -0400

    Creating the main view calls Home

---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// components
+import {
+  TopBar,
+  LogoIcon
+} from '../../components'
+
+function Home() {
+  return (
+    <TopBar>
+      <LogoIcon/>
+    </TopBar>
+  )
+}
+
+export default Home
