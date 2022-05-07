50d9225a559a9554d64b0bc1e0e6eada233eb3d8
mcasanovar <mariocasanovaramirez@gmail.com>
2022-05-07 18:33:55 -0400

    adding axios to utils folder

---
+++ src/utils/axios.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+const axiosClient = axios.create()
+
+axiosClient.defaults.headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json'
+}
+
+export const getRequest = async (URL) => {
+  return axiosClient.get(`/${URL}`).then((response) => response)
+}
+
+export const postRequest = async (URL, payload) => {
+  return axiosClient.post(`/${URL}`, payload).then((response) => response)
+}
