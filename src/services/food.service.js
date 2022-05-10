d0638de2b16b3a0d6289418e1c47cf7803d5a3be
mcasanovar <mariocasanovaramirez@gmail.com>
2022-05-09 23:34:12 -0400

    adding new icons and image to production and api food is enabled

--- src/services/food.service.js
+++ src/services/food.service.js
@@ -1,30 +1,22 @@
 import {
   OK_PROMISE_RETURN,
-  ERROR_PROMISE_RETURN
-  // ITEMS_PER_PAGE,
-  // API_URL,
-  // API_KEY
+  ERROR_PROMISE_RETURN,
+  ITEMS_PER_PAGE,
+  API_URL,
+  API_KEY
 } from '../constants'
-// import { getRequest } from '../utils/axios'
-import fake from '../functions/fakeData.json'
+import { getRequest } from '../utils/axios'
 
 const getRecipes = async (offset, value, maxCalories, cuisines) => {
   try {
-    // const query = value ? `query=${value}` : 'query='
-    // const calories = maxCalories ? `maxCalories=${maxCalories}` : ''
-    // const typeCuisines = cuisines ? `cuisine=${cuisines}` : ''
+    const query = value ? `query=${value}` : 'query='
+    const calories = maxCalories ? `maxCalories=${maxCalories}` : ''
+    const typeCuisines = cuisines ? `cuisine=${cuisines}` : ''
 
-    // const response = await getRequest(
-    //   `${API_URL}recipes/complexSearch?${query}&number=${ITEMS_PER_PAGE}&offset=${offset}&${calories}&${typeCuisines}&addRecipeInformation=true&apiKey=${API_KEY}`
-    // )
-    const response = fake
-    console.log(response)
-    // return {
-    //   status: OK_PROMISE_RETURN,
-    //   data: response.results,
-    //   offset: response.offset,
-    //   totalResults: response.totalResults
-    // }
+    const response = await getRequest(
+      `${API_URL}recipes/complexSearch?${query}&number=${ITEMS_PER_PAGE}&offset=${offset}&${calories}&${typeCuisines}&addRecipeInformation=true&apiKey=${API_KEY}`
+    )
+
     return {
       status: OK_PROMISE_RETURN,
       data: response.data.results,
